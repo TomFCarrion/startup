20f15502e281e718d35dc203b864d9167bf7fbfe
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-06-27 02:20:39 -0300

    XMHttpRequest
    +Add a button below the section to your index.html.
    
    +Attach a click event to the created button which calls a function that gets a response from http://api.icndb.com/jokes/random. Write the response to the section element. Hint: use the XMLHttpRequest to fetch data from the service

--- 01-JS-DOM-APIs/js/topic1.js
+++ 01-JS-DOM-APIs/js/topic1.js
@@ -6,3 +6,27 @@ function fadeInOnLoad(){
     element.classList.remove("hidden");
 
   };
+
+// function setup()
+// {
+//   loadJSON('http://api.icndb.com/jokes/random',getJoke,'jsonp');
+// }
+//
+// function getJoke(data) {
+//   alert(data.value.joke);
+// }
+//
+
+function getData() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status==200)
+    {
+      var response = JSON.parse(xhr.response);
+      document.getElementById("joke").innerHTML = response.value.joke;
+    }
+  }
+  xhr.open("GET","http://api.icndb.com/jokes/random",true);
+  xhr.send();
+}
