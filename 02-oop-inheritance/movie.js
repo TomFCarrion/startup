8a6ed0627611acdd6de0746962641e6744da591a
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-06-30 17:34:40 -0300

    Create a Movie object
    +Create a Movie object:
    +Instantiate some of your favorite movies and play with them in the console.

---
+++ 02-oop-inheritance/movie.js
@@ -0,0 +1,24 @@
+
+
+function Movie (title, year, duration){
+  this.title = title,
+  this.year = year,
+  this.duration = duration
+}
+
+Movie.prototype.play = function play() {
+  console.log(`Playing ${this.title}...`);
+};
+
+Movie.prototype.pause = function pause() {
+  console.log(`${this.title} is paused...`);
+};
+
+Movie.prototype.resume = function resume () {
+  console.log(`Resuming ${this.title}...`);
+};
+
+let pulpFiction = new Movie('Pulp Fiction', 1994, 154);
+let karateKid = new Movie('The Karate Kid ', 1984 , 126 );
+
+pulpFiction.play();
