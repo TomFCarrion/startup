d7db918d8036cbea849b84385ec140424d210008
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-07-04 15:34:44 -0300

    Add social object
    +Create an object called Social with methods share(friendName) and like(friendName)

--- 02-oop-inheritance/usingClasses/movie.js
+++ 02-oop-inheritance/usingClasses/movie.js
@@ -60,8 +60,18 @@ class Logger{
   }
 }
 
+let social = {  // src : https://www.w3schools.com/js/js_objects.asp
+  share: function(friendName){
+    console.log(`Share ${this.title} with ${friendName}.`);
+  },
+  like: function(friendName){
+    console.log(`${friendName} likes ${this.title}.`);
+  }
+};
 
 let pulpFiction = new Movie('Pulp Fiction', 1994, 154);
+
+Object.assign(pulpFiction,social); //  src: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Object/assign
 let logger = new Logger();
 
 
@@ -72,3 +82,6 @@ pulpFiction.on('resume', logger.log);
 pulpFiction.resume();
 pulpFiction.play();
 pulpFiction.pause();
+
+pulpFiction.share('Samuel L. Jackson'); // output: Share Pulp Fiction with Samuel L. Jackson.
+pulpFiction.like('Samuel L. Jackson');  // output: Samuel L. Jackson likes Pulp Fiction with .
